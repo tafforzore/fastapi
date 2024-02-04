72eaf5890b012479f0ec649dc1f66579b619a411
tafforzore <nzomutchawilfrid@gmail.com>
2024-02-04 19:49:08 +0100

    modif

--- database.py
+++ database.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import psycopg2
 
 DATABASE_URL = "postgres://severin:5eDWCV2Y4tkmiY8iBr76pUSVROyl5dwU@dpg-cmugs8ug1b2c73eibk70-a/user_ebsp"
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
+conn = psycopg2.connect(DATABASE_URL)
+try:
+    print("Connected to the database")
+except Exception as e:
+    print(f"Error: {e}")
+finally:
+    conn.close()
