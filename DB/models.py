5668b05af7f512b7ea20d7681e269b530fdca389
tafforzore <nzomutchawilfrid@gmail.com>
2024-02-07 23:32:12 +0100

    ok1

--- DB/models.py
+++ DB/models.py
@@ -8,19 +8,34 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
+    nom = Column(String)
+    second_nom = Column(String)
+    prenom = Column(String)
+    second_prenom = Column(String)
+    username = Column(String)
+    numeros = Column(Integer)
+    sexe = Column(String)
+    email = Column(String)
+    password = Column(String)
+    pays = Column(String)
+    profession = Column(String)
+    date_de_naissance = Column(String)
+    lieu_de_naissance = Column(String)
+    region = Column(String)
+    quartier = Column(String)
     is_active = Column(Boolean, default=True)
 
-    items = relationship("Item", back_populates="owner")
+    ############cles publique#######
+    publique_key = Column(String)
+
 
  
-class Item(Base):
-    __tablename__ = "items"
+# class Item(Base):
+#     __tablename__ = "items"
 
-    id = Column(Integer, primary_key=True)
-    title = Column(String, index=True)
-    description = Column(String, index=True)
-    owner_id = Column(Integer, ForeignKey("users.id"))
+#     id = Column(Integer, primary_key=True)
+#     title = Column(String, index=True)
+#     description = Column(String, index=True)
+#     owner_id = Column(Integer, ForeignKey("users.id"))
 
-    owner = relationship("User", back_populates="items")
+#     owner = relationship("User", back_populates="items")
