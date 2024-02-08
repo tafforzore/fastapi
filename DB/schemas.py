d665b7e89acf2dfa76118b850ad35271042d65ca
tafforzore <nzomutchawilfrid@gmail.com>
2024-02-08 22:51:37 +0100

    ok

--- DB/schemas.py
+++ DB/schemas.py
@@ -1,40 +1,20 @@
 from pydantic import BaseModel
 
-
-class ItemBase(BaseModel):
-    title: str
-    description: str | None = None
-
-
-class ItemCreate(ItemBase):
-    pass
-
-
-class Item(ItemBase):
-    id: int
-    owner_id: int
-
-    class Config:
-        orm_mode = True
-
-
 class UserBase(BaseModel):
     email: str
-    nom : str
-    second_nom : str
-    prenom : str
-    second_prenom : str
+    nom : str | None = None
+    second_nom : str | None = None
+    prenom : str | None = None
+    second_prenom : str | None = None
     username : str
     numeros : int
-    sexe : str
-    email : str
-    password : str
-    pays : str
-    profession : str
-    date_de_naissance : str
-    lieu_de_naissance : str
-    region : str
-    quartier : str
+    sexe : str | None = None
+    pays : str | None = None
+    profession : str | None = None
+    date_de_naissance : str | None = None
+    lieu_de_naissance : str | None = None
+    region : str | None = None
+    quartier : str | None = None
 
 class UserCreate(UserBase):
     password: str
@@ -43,6 +23,6 @@ class UserCreate(UserBase):
 class User(UserBase):
     id: int
     is_active: bool
-    
+
     class Config:
         orm_mode = True
