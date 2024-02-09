b4785af24c66406c30e168c4394e43bef4712dd7
tafforzore <nzomutchawilfrid@gmail.com>
2024-02-09 20:28:07 +0100

    jjn

--- routes/routes.py
+++ routes/routes.py
@@ -15,7 +15,7 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 
 @blueprint.get("/users/", response_model=list[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):  
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
